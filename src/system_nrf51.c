54b91c66a2a5c42fec52735ed2ba0a5fd525f0c3
Vegar Westerlund <vegarwe@gmail.com>
2014-03-05 22:29:40 +0100

    Updated system config files

--- src/system_nrf51.c
+++ src/system_nrf51.c
@@ -11,75 +11,95 @@
  */
 
 
-/**
-A template file for system_device.c is provided by ARM but adapted by the 
-silicon vendor to match their actual device. As a minimum requirement this file 
-must provide a device specific system configuration function and a global 
-variable that contains the system frequency. It configures the device and 
-initializes typically the oscillator (PLL) that is part of the microcontroller 
-device.
-*/
+
+/* NOTE: Template files (including this one) are application specific and therefore expected to 
+   be copied into the application project folder prior to its use! */
 
 #include <stdint.h>
+#include <stdbool.h>
 #include "nrf.h"
 #include "system_nrf51.h"
 
 /*lint ++flb "Enter library region" */
 
-#define __SYSTEM_CLOCK      (__XTAL)
 
-/**
-  System Core Clock Frequency (Core Clock).
-  
-  Contains the system core clock which is the system clock frequency supplied
-  to the SysTick timer and the processor core clock. This variable can be used
-  by the user application to setup the SysTick timer or configure other
-  parameters. It may also be used by debugger to query the frequency of the
-  debug timer or configure the trace clock speed. SystemCoreClock is
-  initialized with a correct predefined value.
+#define __SYSTEM_CLOCK      (16000000UL)     /*!< nRF51 devices use a fixed System Clock Frequency of 16MHz */
 
-  The compiler must be configured to avoid the removal of this variable in case
-  that the application program is not using it. It is important for debug
-  systems that the variable is physically present in memory so that it can be
-  examined to configure the debugger.
-*/
-uint32_t SystemCoreClock = __SYSTEM_CLOCK;
+static bool is_manual_peripheral_setup_needed(void);
+static bool is_disabled_in_debug_needed(void);
 
-/**
-  Sets up the microcontroller system.
 
-  Typically this function configures the oscillator (PLL) that is part of the
-  microcontroller device. For systems with variable clock speed it also updates
-  the variable SystemCoreClock. SystemInit is called from startup_device file
-  before entering main() function.
-*/
-void SystemInit (void) 
+#if defined ( __CC_ARM )
+    uint32_t SystemCoreClock __attribute__((used)) = __SYSTEM_CLOCK;  
+#elif defined ( __ICCARM__ )
+    __root uint32_t SystemCoreClock = __SYSTEM_CLOCK;
+#elif defined   ( __GNUC__ )
+    uint32_t SystemCoreClock __attribute__((used)) = __SYSTEM_CLOCK;
+#endif
+
+void SystemCoreClockUpdate(void)
 {
-    /* If desired, switch off the unused RAM to lower consumption by the use of RAMON register */
-
-    /* Prepare the peripherals for use as indicated by the PAN 27 "System: Manual setup is required
-       to enable the use of peripherals" found at Product Anomaly document version 1.6 found at 
-       https://www.nordicsemi.com/eng/Products/Bluetooth-R-low-energy/nRF51822/PAN-028. The MACRO
-       is defined in the nrf.h file depending on the device defined. */
-    #if MANUAL_PERIPHERAL_SETUP_NEEDED == 1
-        *(uint32_t *)0x40000504 = 0xC007FFDF;
-        *(uint32_t *)0x40006C18 = 0x00008000;
-    #endif  /* MANUAL_PERIPHERAL_SETUP_NEEDED */
-    #ifndef MANUAL_PERIPHERAL_SETUP_NEEDED
-        #error "Make sure you are using latest nrf.h header file."
-    #endif /* MANUAL_PERIPHERAL_SETUP_NEEDED not defined */
+    SystemCoreClock = __SYSTEM_CLOCK;
 }
 
-/**
-  Updates the variable SystemCoreClock and must be called whenever the core
-  clock is changed during program execution.
-  
-  SystemCoreClockUpdate() evaluates the clock register settings and calculates
-  the current core clock.
-*/
-void SystemCoreClockUpdate (void)
+void SystemInit(void)
 {
-  SystemCoreClock = __SYSTEM_CLOCK;
+    /* If desired, switch off the unused RAM to lower consumption by the use of RAMON register.
+       It can also be done in the application main() function. */
+
+    /* Prepare the peripherals for use as indicated by the PAN 26 "System: Manual setup is required
+       to enable the use of peripherals" found at Product Anomaly document for your device found at
+       https://www.nordicsemi.com/. The side effect of executing these instructions in the devices 
+       that do not need it is that the new peripherals in the second generation devices (LPCOMP for
+       example) will not be available. */
+    if (is_manual_peripheral_setup_needed())
+    {
+        *(uint32_t volatile *)0x40000504 = 0xC007FFDF;
+        *(uint32_t volatile *)0x40006C18 = 0x00008000;
+    }
+    
+    /* Disable PROTENSET registers under debug, as indicated by PAN 59 "MPU: Reset value of DISABLEINDEBUG
+       register is incorrect" found at Product Anomaly document four your device found at 
+       https://www.nordicsemi.com/. There is no side effect of using these instruction if not needed. */
+    if (is_disabled_in_debug_needed())
+    {
+        NRF_MPU->DISABLEINDEBUG = MPU_DISABLEINDEBUG_DISABLEINDEBUG_Disabled << MPU_DISABLEINDEBUG_DISABLEINDEBUG_Pos;
+    }
+}
+
+
+static bool is_manual_peripheral_setup_needed(void) 
+{
+    if ((((*(uint32_t *)0xF0000FE0) & 0x000000FF) == 0x1) && (((*(uint32_t *)0xF0000FE4) & 0x0000000F) == 0x0))
+    {
+        if ((((*(uint32_t *)0xF0000FE8) & 0x000000F0) == 0x00) && (((*(uint32_t *)0xF0000FEC) & 0x000000F0) == 0x0))
+        {
+            return true;
+        }
+        if ((((*(uint32_t *)0xF0000FE8) & 0x000000F0) == 0x10) && (((*(uint32_t *)0xF0000FEC) & 0x000000F0) == 0x0))
+        {
+            return true;
+        }
+        if ((((*(uint32_t *)0xF0000FE8) & 0x000000F0) == 0x30) && (((*(uint32_t *)0xF0000FEC) & 0x000000F0) == 0x0))
+        {
+            return true;
+        }
+    }
+    
+    return false;
+}
+
+static bool is_disabled_in_debug_needed(void) 
+{
+    if ((((*(uint32_t *)0xF0000FE0) & 0x000000FF) == 0x1) && (((*(uint32_t *)0xF0000FE4) & 0x0000000F) == 0x0))
+    {
+        if ((((*(uint32_t *)0xF0000FE8) & 0x000000F0) == 0x40) && (((*(uint32_t *)0xF0000FEC) & 0x000000F0) == 0x0))
+        {
+            return true;
+        }
+    }
+    
+    return false;
 }
 
 /*lint --flb "Leave library region" */
