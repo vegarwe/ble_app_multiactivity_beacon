e7453ea9f99c5427767f313d25822fcbab996185
Vegar Westerlund <vegarwe@gmail.com>
2014-03-05 22:59:39 +0100

    Start work on running an application level radio protocol

---
+++ include/app_ibeacon.h
@@ -0,0 +1,8 @@
+#ifndef APP_IBEACON_H__
+#define APP_IBEACON_H__
+
+void app_ibeacon_sd_evt_signal_handler(uint32_t event)
+void app_ibeacon_setup(void)
+void app_ibeacon_start(void)
+
+#endif // APP_IBEACON_H__
